a49f2214e91212f36c4d03beabc0593f05ff6c2c
Igouist <Ursaliker@gmail.com>
2021-07-10 01:06:30 +0800

    Ref: 初步拆分股票查詢的 Event

--- MyDiscordBot/App.cs
+++ MyDiscordBot/App.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
 using MyDiscordBot.Crawler;
 using MyDiscordBot.TokenGetter;
 using System;
@@ -65,37 +66,45 @@ namespace MyDiscordBot
                 TokenType = TokenType.Bot
             });
 
-            discord.MessageCreated += async (s, e) =>
-            {
-                if (e.Message.Content.ToLower().StartsWith("getstock"))
-                {
-                    var content = e.Message.Content;
-                    var inputs = content.Split(':');
-                    
-                    if (inputs.Length > 1 && Int16.TryParse(inputs[1], out var idx))
-                    {
-                        var stock = await this._crawler.Fetch(idx);
-                        var rn = Environment.NewLine;
-
-                        var message =
-                            $"名稱: {stock.Name}{rn}" +
-                            $"成交: {stock.FinalPrice}{rn}" +
-                            $"買進: {stock.BuyPrice}{rn}" +
-                            $"賣出: {stock.SellPrice}{rn}" +
-                            $"漲跌: {stock.UpDown.Trim()}{rn}" +
-                            $"張數: {stock.Lot}{rn}" +
-                            $"昨收: {stock.YesterdayPrice}{rn}" +
-                            $"開盤: {stock.OpeningPrice}{rn}" +
-                            $"最高: {stock.HighestPrice}{rn}" +
-                            $"最低: {stock.LowestPrice}{rn}";
-
-                        await e.Message.RespondAsync(message);
-                    }
-                }
-            };
+            discord.MessageCreated += FetchStockInfo;
 
             await discord.ConnectAsync();
             await Task.Delay(-1);
+        }
+
+        /// <summary>
+        /// 查詢股票功能
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        private async Task FetchStockInfo(DiscordClient client, MessageCreateEventArgs args)
+        {
+            if (args.Message.Content.ToLower().StartsWith("getstock"))
+            {
+                var content = args.Message.Content;
+                var inputs = content.Split(':');
+
+                if (inputs.Length > 1 && Int16.TryParse(inputs[1], out var idx))
+                {
+                    var stock = await this._crawler.Fetch(idx);
+                    var rn = Environment.NewLine;
+
+                    var message =
+                        $"名稱: {stock.Name}{rn}" +
+                        $"成交: {stock.FinalPrice}{rn}" +
+                        $"買進: {stock.BuyPrice}{rn}" +
+                        $"賣出: {stock.SellPrice}{rn}" +
+                        $"漲跌: {stock.UpDown.Trim()}{rn}" +
+                        $"張數: {stock.Lot}{rn}" +
+                        $"昨收: {stock.YesterdayPrice}{rn}" +
+                        $"開盤: {stock.OpeningPrice}{rn}" +
+                        $"最高: {stock.HighestPrice}{rn}" +
+                        $"最低: {stock.LowestPrice}{rn}";
+
+                    await args.Message.RespondAsync(message);
+                }
+            }
         }
     }
 }

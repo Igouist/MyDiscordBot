b73101adadbe291fb04c00953c83aad0eff97853
Igouist <Ursaliker@gmail.com>
2021-07-09 23:38:59 +0800

    Ref: 拆分簡易 DI

---
+++ MyDiscordBot/App.cs
@@ -0,0 +1,75 @@
+﻿using DSharpPlus;
+using MyDiscordBot.TokenGetter;
+using System;
+using System.Threading.Tasks;
+
+namespace MyDiscordBot
+{
+    /// <summary>
+    /// 主要應用程式
+    /// </summary>
+    public class App
+    {
+        private ITokenService _tokenService;
+
+        /// <summary>
+        /// Init
+        /// </summary>
+        public App(ITokenService tokenService)
+        {
+            this._tokenService = tokenService;
+        }
+
+        /// <summary>
+        /// 主流程
+        /// </summary>
+        /// <returns></returns>
+        public async Task Run()
+        {
+            Console.WriteLine($"正在啟動 Bot...");
+            Console.WriteLine($"正在取得 Token...");
+
+            var token = await _tokenService.Get();
+
+            Console.WriteLine($"已取得 Token:{token}");
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                return;
+            }
+
+            Console.WriteLine($"正在連接 Discord...");
+
+            await MainAsync(token);
+
+            Console.WriteLine($"Bot 已關閉");
+        }
+
+
+        /// <summary>
+        /// 主要 Bot 部份
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        static async Task MainAsync(string token)
+        {
+            // 參考資料: https://dsharpplus.github.io/articles/basics/first_bot.html
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = token,
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("test"))
+                {
+                    await e.Message.RespondAsync("測你娘啦");
+                }
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
--- MyDiscordBot/Program.cs
+++ MyDiscordBot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus;
+using Microsoft.Extensions.DependencyInjection;
 using MyDiscordBot.TokenGetter;
 using System;
 using System.Threading.Tasks;
@@ -9,44 +10,17 @@ namespace MyDiscordBot
     {
         static async Task Main(string[] args)
         {
-            Console.WriteLine($"正在啟動 Bot...");
-            Console.WriteLine($"正在取得 Token...");
+            var service = new ServiceCollection();
+            Startup(service);
 
-            ITokenService tokenService = new FileTokenService();
-            var token = await tokenService.Get();
-
-            Console.WriteLine($"已取得 Token:{token}");
-            Console.WriteLine($"正在連接 Discord...");
-
-            await MainAsync(token);
-
-            Console.WriteLine($"Bot 已關閉");
+            var provider = service.BuildServiceProvider();
+            await provider.GetRequiredService<App>().Run();
         }
 
-        /// <summary>
-        /// 主要 Bot 部份
-        /// </summary>
-        /// <param name="token"></param>
-        /// <returns></returns>
-        static async Task MainAsync(string token)
+        static void Startup(ServiceCollection services)
         {
-            // 參考資料: https://dsharpplus.github.io/articles/basics/first_bot.html
-            var discord = new DiscordClient(new DiscordConfiguration()
-            {
-                Token = token,
-                TokenType = TokenType.Bot
-            });
-
-            discord.MessageCreated += async (s, e) =>
-            {
-                if (e.Message.Content.ToLower().StartsWith("test"))
-                {
-                    await e.Message.RespondAsync("測你娘啦");
-                }
-            };
-
-            await discord.ConnectAsync();
-            await Task.Delay(-1);
+            services.AddScoped<ITokenService, FileTokenService>();
+            services.AddTransient<App>();
         }
     }
 }
